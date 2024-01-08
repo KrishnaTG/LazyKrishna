f4b5cd1d11db63bbbcc1670c3c53cb425f74533b
Krishna Singh <152683218+KrishnaTG@users.noreply.github.com>
2024-01-08 18:22:44 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -3,7 +3,8 @@
 
 # https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK, TUTORIAL, IS_TUTORIAL
+import datetime
+from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT
 
 class Database:
     
@@ -13,17 +14,23 @@ class Database:
         self.col = self.db.users
         self.grp = self.db.groups
 
-
     def new_user(self, id, name):
-        return dict(
+        return dict(    
             id = id,
             name = name,
+            _id=int(id),                                   
+            file_id=None,
+            caption=None,
+            lazy_caption=None,
+            join_date=datetime.date.today().isoformat(),
+            apply_caption=True,
+            upload_as_doc=False,
+            thumbnail=None,
             ban_status=dict(
                 is_banned=False,
                 ban_reason="",
             ),
         )
-
 
     def new_group(self, id, title):
         return dict(
@@ -85,8 +92,6 @@ class Database:
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
         return b_users, b_chats
-    
-
 
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
@@ -97,7 +102,6 @@ class Database:
         chat = await self.grp.find_one({'id':int(chat)})
         return False if not chat else chat.get('chat_status')
     
-
     async def re_enable_chat(self, id):
         chat_status=dict(
             is_disabled=False,
@@ -117,15 +121,7 @@ class Database:
             'imdb': IMDB,
             'spell_check': SPELL_CHECK_REPLY,
             'welcome': MELCOW_NEW_USERS,
-            'auto_delete': AUTO_DELETE,
-            'auto_ffilter': AUTO_FFILTER,
-            'max_btn': MAX_BTN,
-            'template': IMDB_TEMPLATE,
-            'shortlink': SHORTLINK_URL,
-            'shortlink_api': SHORTLINK_API,
-            'is_shortlink': IS_SHORTLINK,
-            'tutorial': TUTORIAL,
-            'is_tutorial': IS_TUTORIAL
+            'template': IMDB_TEMPLATE
         }
         chat = await self.grp.find_one({'id':int(id)})
         if chat:
@@ -149,9 +145,67 @@ class Database:
     async def get_all_chats(self):
         return self.grp.find({})
 
-
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
+    
+    # Credit @LazyDeveloper.
+    # Please Don't remove credit.
+        # Born to make history @LazyDeveloper ! => Remember this name forever <=
+
+    # Thank you LazyDeveloper for helping us in this Journey
+    # Just for url Uploading feature
+
+    async def set_apply_caption(self, id, apply_caption):
+        await self.col.update_one({'id': id}, {'$set': {'apply_caption': apply_caption}})
+
+    async def get_apply_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('apply_caption', True)
+
+    async def set_upload_as_doc(self, id, upload_as_doc):
+        await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
+
+    async def get_upload_as_doc(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('upload_as_doc', False)
+
+    async def set_lazy_thumbnail(self, id, thumbnail):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_lazy_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
+    async def get_lazy_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('lazy_caption', None)
+
+    async def get_user_data(self, id) -> dict:
+        user = await self.col.find_one({'id': int(id)})
+        return user or None
+        
+    # Thank you LazyDeveloper for helping us in this Journey
+    # Just for renamer @Krishna00P 
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'id': int(id)}, {'$set': {'file_id': file_id}})
+        
+    async def get_thumbnail(self, id):
+        try:
+            thumbnail = await self.col.find_one({'id': int(id)})
+            if thumbnail:
+                return thumbnail.get('file_id')
+            else:
+                return None
+        except Exception as e:
+            print(e)
+    # Born to make history @LazyDeveloper ! => Remember this name forever <=
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': int(id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
